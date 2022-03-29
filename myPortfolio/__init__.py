f1d4a924a2ac1cc2100ba40b263c78fbdb8c642a
Philip Zhu <philipzhuhan@gmail.com>
2022-03-29 08:38:49 +0800

    removed mail related codes

--- myPortfolio/__init__.py
+++ myPortfolio/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_mail import Mail
+# from flask_mail import Mail
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
